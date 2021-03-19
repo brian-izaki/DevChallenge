bdf41c06e217adca9ae6b768a7d6ead92feff98a
Brian_Izaki <bkizaki@outlook.com>
2021-03-19 17:22:21 -0300

    refactor

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const menu = document.querySelector('.menu')
-const openMenu = document.querySelector('nav [data-hamburguer="open"]')
-const closeMenu = document.querySelector('.menu [data-hamburguer="close"]')
+const menuMobile = document.querySelector('.menu')
+const btnOpenMenu = document.querySelector('nav [data-hamburguer="open"]')
+const btnCloseMenu = document.querySelector('.menu [data-hamburguer="close"]')
 
 function toggleMenu(e) {
-  menu.classList.toggle('ativo')  
+  menuMobile.classList.toggle('ativo')  
 }
 
-openMenu.addEventListener('click', toggleMenu)
-closeMenu.addEventListener('click', toggleMenu)
+btnOpenMenu.addEventListener('click', toggleMenu)
+btnCloseMenu.addEventListener('click', toggleMenu)
